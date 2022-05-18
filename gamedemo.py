4b6c07f931a800e1711586be60e78bd38ff42e7d
gengzichen <2020905037@qq.com>
2022-05-18 13:22:16 +0800

    add game demo

---
+++ gamedemo.py
@@ -0,0 +1,49 @@
+from Game import *
+from Player import *
+from Dealer import * 
+from Card import *
+from RoyalCard import *
+
+Zen = UserAccount('Zen', 'gengzichenchin@gmail.com', 2000)
+Player = Zen.newPlayer()
+Dealer = Dealer()
+mygame = Game(Player, Dealer)
+
+print('Welcome to the Royal Card Game')
+print('-'*50)
+
+while(mygame.end==False):
+    print('='*60)
+    print('Your coin:', mygame.Player.coin)
+    print('POT:', mygame.Pot)
+    print('Dealer Card:', [card.name for card in mygame.DealerCard], 'Dealer Bet:', mygame.Dealer.bet)
+    if len(mygame.PublicCard) != 0:
+        print('Plublic Card:', [card.name for card in mygame.PublicCard])
+    print('Your Card:', [card.name for card in mygame.PlayerCard], 'Player Bet:', mygame.Player.bet)
+    print('='*60)
+    print('You can', [i for i in mygame.actions])
+    input_action = input('Input your action:')
+    input_action = input_action.split(' ')
+    act = ''
+    bet = 0
+    while input_action[0] not in mygame.actions:
+        print('You can', [i for i in mygame.actions])
+        input_action = input('Input your action:')
+        input_action = input_action.split(' ')
+    act = input_action[0]
+    if len(input_action) > 1:
+        bet = int(input_action[1])
+    mygame.play(action=act, bet = bet)
+
+print('='*60)
+print('Your coin:', mygame.Player.coin)
+print('POT:', mygame.Pot)
+print('Dealer Card:', [card.name for card in mygame.DealerCard], 'Dealer Bet:', mygame.Dealer.bet)
+if len(mygame.PublicCard) != 0:
+    print('Plublic Card:', [card.name for card in mygame.PublicCard])
+print('Your Card:', [card.name for card in mygame.PlayerCard], 'Player Bet:', mygame.Player.bet)
+print('='*60)
+
+print('Game over! You', 'won' if mygame.result >=0 else 'lost', mygame.result)
+
+
