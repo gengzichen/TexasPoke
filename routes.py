3e39a35b79256734cb0cded7e6ecaa85cb71a5f9
YicongMa <84171961+YicongMa@users.noreply.github.com>
2022-05-22 17:23:03 +0800

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from crypt import methods
+from collections import UserList
+from datetime import datetime
 from flask import jsonify, redirect, render_template, request
 from app import app,db
 from app import models
@@ -11,7 +12,6 @@ from app.api.RoyalCard import *
 
 from app.forms import LoginForm
 
-player = ["mycc",100]
 
 @app.route("/")
 @app.route("/welcome")
@@ -30,11 +30,19 @@ def login_to():
     users=models.User.query.filter_by(username=username).all()
     for user in users:
         if user.check_password(password):
+            if user.last_login_time != datetime.now().strftime("%x"):
+                user.coin +=1000
+                user.last_login_time = datetime.now().strftime("%x")
+                db.session.commit()
             global player 
             player = user.user_info()
             return redirect('/game')
     
     return render_template('login.html',error="The username or password you enter is not corrent. Please enter again")
+
+@app.route("/logout")
+def logout():
+    return redirect("/login")
 
 @app.route("/register",methods=['POST'])
 def register():
@@ -45,8 +53,10 @@ def register():
     for user in users:
         if username==user.username:
             return render_template('login.html',error="The username have been registered, Please use another one.")
-    
-    user=models.User(username,password,1000)
+    if len(password)<8:
+        return render_template('login.html',error="The password you create is too short. Please create again.")
+
+    user = models.User(username,password,1000,time=datetime.now().strftime("%x"))
     db.session.add(user)
     db.session.commit()
 
@@ -54,28 +64,38 @@ def register():
 
 @app.route("/game")
 def game():
+    
     return render_template('game.html',coin=player[1])
+
+@app.route("/help")
+def help():
+    return render_template('help.html')
 
 @app.route("/level")
 def level():
     users = models.User.query.order_by(models.User.coin.desc()).all()
 
-    level={}
-    level["first_place"]=users[0].username
-    level["first_place_c"]=users[0].coin
-    level["second_place"]=users[1].username
-    level["second_place_c"]=users[1].coin
-    level["third_place"]=users[2].username
-    level["third_place_c"]=users[2].coin
+    i=0
+    user_info_list =[]
+    for user in users:
+        i += 1
+        user_info=user.get_user_info()
+        user_info['no'] = i
+        if user_info['username']==player[0]:
+            user_info['isplayer']=True
+        else:
+            user_info['isplayer']=False
 
-    three_top_name=[users[0].username,users[1].username,users[2].username]
-    # if player.username in three_top_name:
-    #     level["congrations"]="congrations"
+        user_info_list.append(user_info)
 
-    return render_template('level.html',level=level)
+    
+    print(user_info_list)
+
+    return render_template('level.html',users=user_info_list)
 
 
-
+    
+#Start Game
 @app.route("/nexthand")
 def play():
     
@@ -88,9 +108,11 @@ def play():
 
     game_info={}
     game_info["player_coin"] = mygame.Player.coin
+    player[1]=mygame.Player.coin
+
     game_info["pot"] = mygame.Pot
 
-    game_info["state"] = mygame.state
+    # game_info["state"] = mygame.state
 
     player_card = [card.name for card in mygame.PlayerCard]
     game_info["player_first_card"] = player_card[0]
@@ -105,10 +127,7 @@ def play():
     game_info["all_action"] = mygame.action_list
     game_info["actions"] = mygame.actions
 
-
-    public_card = [card.name for card in mygame.PublicCard]
-    print(public_card)
-    # game_info["public"] = 
+    game_info["message"] = mygame.Dealer.message
 
     print(game_info)
     return jsonify(game_info)
@@ -120,6 +139,12 @@ def call():
     game_info={}
     game_info["end"] = mygame.end
     game_info["player_coin"] = mygame.Player.coin
+    player[1]=mygame.Player.coin
+
+    user=models.User.query.filter_by(username=player[0]).first()
+    user.coin=mygame.Player.coin
+    db.session.commit()
+
     game_info["pot"] = mygame.Pot
 
     public_card = [card.name for card in mygame.PublicCard]
@@ -131,6 +156,9 @@ def call():
 
     game_info["player_bet"] = mygame.Player.bet
     game_info["dealer_bet"] = mygame.Dealer.bet
+
+    game_info["player_message"] = mygame.Player.message
+    game_info["dealer_message"] = mygame.Dealer.message
 
     if(mygame.end==False):
         game_info["all_action"] = mygame.action_list
@@ -155,6 +183,12 @@ def minbet():
     mygame.play("minBet",0)
     game_info={}
     game_info["player_coin"] = mygame.Player.coin
+    player[1]=mygame.Player.coin
+    game_info["pot"] = mygame.Pot
+
+    user=models.User.query.filter_by(username=player[0]).first()
+    user.coin=mygame.Player.coin
+    db.session.commit()
 
     game_info["player_bet"] = mygame.Player.bet
     game_info["dealer_bet"] = mygame.Dealer.bet
@@ -166,10 +200,17 @@ def minbet():
     game_info["all_action"] = mygame.action_list
     game_info["actions"] = mygame.actions
 
+    game_info["player_message"] = mygame.Player.message
+    game_info["dealer_message"] = mygame.Dealer.message
+
     if mygame.result>=0:
         game_info["result"] = "You Win " + str(mygame.result)
     else:
         game_info["result"] = "You Lost " + str(abs(mygame.result))
+
+    dealer_card = [card.name for card in mygame.DealerCard]
+    game_info["dealer_first_card"] = dealer_card[0]
+    game_info["dealer_second_card"] = dealer_card[1]
 
     print(game_info)
 
@@ -182,15 +223,31 @@ def bet():
     mygame.play("bet",int(betnumber))
     game_info={}
     game_info["player_coin"] = mygame.Player.coin
+    player[1]=mygame.Player.coin
+    game_info["pot"] = mygame.Pot
+
+    user=models.User.query.filter_by(username=player[0]).first()
+    user.coin=mygame.Player.coin
+    db.session.commit()
+
+
     game_info["end"] = mygame.end
 
     public_card = [card.name for card in mygame.PublicCard]
     game_info["public_card"] = public_card
 
     game_info["player_bet"] = mygame.Player.bet
+    game_info["dealer_bet"] = mygame.Dealer.bet
 
     game_info["all_action"] = mygame.action_list
     game_info["actions"] = mygame.actions
+
+    game_info["player_message"] = mygame.Player.message
+    game_info["dealer_message"] = mygame.Dealer.message
+
+    dealer_card = [card.name for card in mygame.DealerCard]
+    game_info["dealer_first_card"] = dealer_card[0]
+    game_info["dealer_second_card"] = dealer_card[1]
     
 
     if mygame.result>=0:
@@ -208,6 +265,17 @@ def check():
     mygame.play("check",bet=0)
     game_info={}
     game_info["player_coin"] = mygame.Player.coin
+    player[1]=mygame.Player.coin
+    game_info["pot"] = mygame.Pot
+
+    user=models.User.query.filter_by(username=player[0]).first()
+    user.coin=mygame.Player.coin
+    db.session.commit()
+
+    game_info["player_bet"] = mygame.Player.bet
+    game_info["dealer_bet"] = mygame.Dealer.bet
+
+
     game_info["end"] = mygame.end
 
     public_card = [card.name for card in mygame.PublicCard]
@@ -215,6 +283,20 @@ def check():
 
     game_info["all_action"] = mygame.action_list
     game_info["actions"] = mygame.actions
+
+    game_info["player_message"] = mygame.Player.message
+    game_info["dealer_message"] = mygame.Dealer.message
+
+    if mygame.result>=0:
+        game_info["result"] = "You Win " + str(mygame.result)
+    else:
+        game_info["result"] = "You Lost " + str(abs(mygame.result))
+
+    dealer_card = [card.name for card in mygame.DealerCard]
+    game_info["dealer_first_card"] = dealer_card[0]
+    game_info["dealer_second_card"] = dealer_card[1]
+
+
     print(game_info)
 
     return jsonify(game_info)
@@ -224,6 +306,13 @@ def allin():
     mygame.play("allIn",0)
     game_info={}
     game_info["player_coin"] = mygame.Player.coin
+    player[1]=mygame.Player.coin
+    game_info["pot"] = mygame.Pot
+
+    user=models.User.query.filter_by(username=player[0]).first()
+    user.coin=mygame.Player.coin
+    db.session.commit()
+
     game_info["player_bet"] = mygame.Player.bet
     game_info["dealer_bet"] = mygame.Dealer.bet
 
@@ -231,6 +320,9 @@ def allin():
 
     public_card = [card.name for card in mygame.PublicCard]
     game_info["public_card"] = public_card
+
+    game_info["player_message"] = mygame.Player.message
+    game_info["dealer_message"] = mygame.Dealer.message
 
     if(mygame.end==False):
         game_info["all_action"] = mygame.action_list
@@ -256,6 +348,15 @@ def fold():
     mygame.play("fold",0)
     game_info={}
     game_info["player_coin"] = mygame.Player.coin
+    player[1]=mygame.Player.coin
+    game_info["pot"] = mygame.Pot
+
+    game_info["player_bet"] = mygame.Player.bet
+    game_info["dealer_bet"] = mygame.Dealer.bet
+
+    user=models.User.query.filter_by(username=player[0]).first()
+    user.coin=mygame.Player.coin
+    db.session.commit()
 
     public_card = [card.name for card in mygame.PublicCard]
     game_info["public_card"] = public_card
@@ -264,19 +365,13 @@ def fold():
     game_info["dealer_first_card"] = dealer_card[0]
     game_info["dealer_second_card"] = dealer_card[1]
 
+    game_info["player_message"] = mygame.Player.message
+    game_info["dealer_message"] = mygame.Dealer.message
+
     if mygame.result>=0:
         game_info["result"] = "You Win " + str(mygame.result)
     else:
         game_info["result"] = "You Lost " + str(abs(mygame.result))
 
     return jsonify(game_info)
-
-
-
-
-
-
-
-
-
 
